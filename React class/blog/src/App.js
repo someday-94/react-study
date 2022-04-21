d887e4ccd693127a09015ed25c9611a760d56f39
Jo YoHan <whdygks210@naver.com>
2022-04-21 18:23:16 +0900

    [update] Part1 - 블로그 제작 & 기초문법

--- React class/blog/src/App.js
+++ React class/blog/src/App.js
@@ -2,45 +2,117 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [title, updateTitle] = useState(["프로젝트 1", "프로젝트 2", "프로젝트 3"]);
-  let [good, updateGood] = useState(0);
+  let [postArr, setPostArr] = useState([["다 프로젝트", 0], ["나 프로젝트", 0], ["가 프로젝트", 0]]);
+  let [isShowModal, setIsShowModal] = useState(false);
 
-  function changeTitle() {
-    let newTitle = [...title];
-    newTitle[0] = "프로젝트 4";
-    updateTitle(newTitle);
+  function changePostArr() {
+    let newTitle = [...postArr];
+    newTitle[0] = "프로젝트";
+    setPostArr(newTitle);
+  }
+
+  function sortPostArr() {
+    let newTitle = [...postArr];
+
+    newTitle.sort();
+
+    setPostArr(newTitle);
+  }
+
+  function showModal() {
+    if (isShowModal === true) {
+      setIsShowModal(false);
+    } else {
+      setIsShowModal(true);
+    }
+  }
+
+  function plusGoodCount(index) {
+    let newContentArr = [...postArr];
+    newContentArr[index][1] += 1;
+    setPostArr(newContentArr)
   }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 Blog</div>
+        <div>Dev Blog</div>
       </div>
-      <button onClick={changeTitle}> change </button>
+      <button onClick={changePostArr}> change </button>
+      <button onClick={sortPostArr}> sort </button>
 
-      <div className="list">
-        <h3>
-          {title[0]}{" "}
+      {postArr.map(function (post, index) {
+        return (
+          <div className="list" key={index}>
+            <h4
+              onClick={() => {
+                showModal(true);
+              }}
+            >
+              {post[0]}
+              <span
+                onClick={() => {
+                  plusGoodCount(index);
+                }}
+              >
+                👍
+              </span>
+              {post[1]}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+
+      <>
+        {/* 
+      {titleArr.map(function (title, index) {
+        return (
+          <div className="list">
+          <h4>
+          {titleArr[index]}
           <span
-            onClick={() => {
-              updateGood(good + 1);
-            }}
+          onClick={() => {
+            setGoodCount(goodCount + 1);
+          }}
           >
-            👍
-          </span>{" "}
-          {good}
-        </h3>
-        <p>2월 17일 발행</p>
-        <hr />
-        <h3>{title[1]}</h3>
-        <p>2월 18일 발행</p>
-        <hr />
-        <h3>{title[2]}</h3>
-        <p>2월 19일 발행</p>
-        <hr />
-      </div>
+          👍
+          </span>
+          {goodCount}
+          </h4>
+            <p>2월 17일 발행</p>
+          </div>
+          );
+        })} 
+      */}
+      </>
+
+      {isShowModal === true ? <Modal color="whitesmoke" background={'gray'} title={postArr}/> : null}
     </div>
   );
 }
+
+// 함수 첫 글자는 대문자
+function Modal(props) {
+  return (
+    <div className="modal" style={{ background: props.background, color: props.color }}>
+      <h4>{props.title}</h4>
+      <p>date</p>
+      <p>content</p>
+      <button>글수정</button>
+    </div>
+  );
+}
+
+// 이렇게 const로 Mothod를 작성해 놓으면 나중에 실수로 수정하는 일을 방지해 준다.
+const Modal2 = () => {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+};
 
 export default App;
